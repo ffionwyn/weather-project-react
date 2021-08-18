500e8fc8242371960d3d48f6b1f13918ab0e0b50
ffionwyn <78923167+ffionwyn@users.noreply.github.com>
2021-08-18 15:45:57 +0100

    added conversion f/c
    added conversion buttons for fahrenheit/celsius

--- src/search.js
+++ src/search.js
@@ -6,6 +6,7 @@ export default function Search() {
   const [city, setCity] = useState("");
   const [loaded, setLoaded] = useState(false);
   const [weather, setWeather] = useState({});
+  const [temperatureUnit, setTemperatureUnit] = useState("metric");
 
   function displayWeather(response) {
     setLoaded(true);
@@ -20,9 +21,16 @@ export default function Search() {
     event.preventDefault();
     if (city.length > 0) {
       let apiKey = "6b953ee073bb6d456951bd28c1caeaaf";
-      let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+      let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${temperatureUnit}`;
       axios.get(apiUrl).then(displayWeather);
     }
+  }
+
+  function getTemperature(celsius) {
+    if (temperatureUnit === "metric") {
+      return celsius;
+    }
+    return (celsius * 9) / 5 + 32;
   }
 
   function updateCity(event) {
@@ -47,11 +55,29 @@ export default function Search() {
         {form}
         <ul>
           <li>{city} </li>
-          <li> {Math.round(weather.temperature)}°C</li>
+          <li> {Math.round(getTemperature(weather.temperature))}°C</li>
           <li> {weather.description}</li>
           <li>{weather.humidity}% Humidity</li>
           <li>{weather.wind}mph Wind</li>
         </ul>
+        <input
+          type="radio"
+          id="metric"
+          name="temperature-unit"
+          value="metric"
+          checked={temperatureUnit === "metric"}
+          onChange={() => setTemperatureUnit("metric")}
+        />
+        <label htmlFor="metric">°C</label>
+        <input
+          type="radio"
+          id="imperial"
+          name="temperature-unit"
+          value="imperial"
+          checked={temperatureUnit === "imperial"}
+          onChange={() => setTemperatureUnit("imperial")}
+        />
+        <label htmlFor="imperial">°F</label>
       </div>
     );
   } else {
